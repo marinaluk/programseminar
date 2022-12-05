ab1c5ec2037a230f821fdd9926165fa8b94bf5a3
unknown <C:\Users\Marina\AppData\Roaming\The Bat!>
2022-12-05 15:16:41 +0800

    Добавлены новые папки по 2 семинару

---
+++ hw/3/task 1/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+сonsole.Write("Input fiveDigitNumber : ");
+int fiveDigitNumber = Convert.ToInt32(Console.ReadLine());
